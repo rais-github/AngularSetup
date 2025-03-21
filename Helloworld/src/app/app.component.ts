4beea65dabc6dc872511b2998f327f1de8806e2c
Himanshu Rai <himanshu2003rai@gmail.com>
2025-03-21 15:23:35 +0530

    validate Name

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   imgUrl = 'BL_logo_square_jpg.jpg';
   url = 'https://www.bridgelabz.com/';
   username: string = '';
+  nameError: string = '';
   ngOnInit() {
     this.title = 'Hello from Bridgelabz';
   }
@@ -19,5 +20,14 @@ export class AppComponent {
   onClick($event: any) {
     console.log('Save button is clicked!', $event);
     window.open(this.url, '_blank');
+  }
+  onInput($event: any) {
+    console.log('Change Event Occured!', $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.username)) {
+      this.nameError = '';
+      return;
+    }
+    this.nameError = 'Name is Incorrect!';
   }
 }
