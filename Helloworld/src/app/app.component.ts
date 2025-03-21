6bd9cd58ef40a5d89b65afb5b32fe5e7617b98c1
Himanshu Rai <himanshu2003rai@gmail.com>
2025-03-21 14:22:39 +0530

    Event Binding

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -10,7 +10,13 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Helloworld';
   imgUrl = 'BL_logo_square_jpg.jpg';
+  url = 'https://www.bridgelabz.com/';
   ngOnInit() {
     this.title = 'Hello from Bridgelabz';
+  }
+
+  onClick($event: any) {
+    console.log('Save button is clicked!', $event);
+    window.open(this.url, '_blank');
   }
 }
